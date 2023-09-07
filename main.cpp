47c8083a8d4c3ff235571599a0a16357d9a593f2
adoyeong <puppy2621235@korea.ac.kr>
2023-09-08 05:17:04 +0900

    Add test code

--- main.cpp
+++ main.cpp
@@ -6,5 +6,11 @@ int main()
 	char a[17] = "0123456789abcdef";
 	char b[17] = "0123456789abcdef";
 	byte* c = des(a, b);
+	for(int i=1; i<65; i++)
+	{
+		printf("%d", c[i]);
+		if(i%4==0) printf(" ");
+	}
+	printf("\n");
 	return 0;
 }
